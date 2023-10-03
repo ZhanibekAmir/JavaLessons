081b69534a1e175d33ccb5e1e5ee701f18f06f06
ZhanibekAmir <137271120+ZhanibekAmir@users.noreply.github.com>
2023-10-03 19:45:29 +0600

    Create l1.java

---
+++ l1.java
@@ -0,0 +1,11 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        String massive[]={"est","takoe","dokonsa","ku","ee"};
+        for(int i=0;i < massive.length-1;i++){
+            System.out.print(massive[i]+"-");
+        } System.out.print(massive[massive.length-1]);
+    }
+}
